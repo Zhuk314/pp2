b751d5f9703413ae0518f5206e3275c76edca1ff
Yurii Zhuk <iamyuriizhuk@gmail.com>
2021-04-15 12:44:01 -0400

    Create a file, turn on error reporting.

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// Author: Yurii Zhuk
+// Date: 04/15/2021
+// File URL: http://yzhuk.greenriverdev.com/328/pp2/
+// Pair Program 2
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2.</title>
+</head>
+<body>
+<?php
+echo "<p>PHP Array Practice</p>";
+
+?>
+</body>
+</html>
