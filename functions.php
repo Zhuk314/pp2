2e93c6ef342310b8751d1ae4e7a43c8759774f78
Yurii Zhuk <iamyuriizhuk@gmail.com>
2021-04-15 17:54:20 -0400

    Write function 'largest'.

--- functions.php
+++ functions.php
@@ -1,6 +1,26 @@
 <?php
-function printArr($array) {
+//p
+function printArr(array $array) {
+    echo "{";
+
     foreach($array as $value){
-        echo "<p>$value</p>";
+        echo "<span> $value </span>";
     }
+
+    echo "}";
+}
+
+// Return the largest value of array
+function largest(array $array) {
+    // create a variable to hold hte largest value
+    $largest = $array[0];
+
+    // loop trough the array to find largest value
+    for($i=1; $i<sizeof($array); $i++) {
+        if($i > $largest){
+            $largest = $i;
+        }
+    }
+
+    return $largest;
 }
--- index.php
+++ index.php
@@ -17,19 +17,34 @@ error_reporting(E_ALL);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
     <title>Pair Program 2.</title>
 </head>
 <body>
+<div class="container jumbotron">
 <?php
-echo "<p>PHP Array Practice</p>";
+echo "<h1 class='text-center'>PHP Array Practice</h1>";
 include 'functions.php';
 
-// 2. Define an array $numbers...
+// 2.
+// Define an array $numbers...
+echo "<h3>The array: </h3>";
 $numbers = array(7,9,8,9,8,8,6);
 
-
-
 printArr($numbers);
+
+
+// 3.
+// Call the function largest and print the result
+echo "<h3>The largest number: </h3>";
+$largestNum = largest($numbers);
+echo "<p>The largest number is: $largestNum</p>";
+
+
 
 
 
@@ -37,5 +52,6 @@ printArr($numbers);
 
 
 ?>
+</div>
 </body>
 </html>
